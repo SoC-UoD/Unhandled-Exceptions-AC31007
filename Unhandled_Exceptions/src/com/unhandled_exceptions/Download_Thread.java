61568494df7d1f2eda09ac69ad6acdc93259c3b2
Scott Reid <sfreid@dundee.ac.uk>
2012-11-12 18:00:06 +0000

    added download thread and fixed small code error

--- Unhandled_Exceptions/src/com/unhandled_exceptions/Download_Thread.java
+++ Unhandled_Exceptions/src/com/unhandled_exceptions/Download_Thread.java
@@ -1,62 +1,98 @@
 package com.unhandled_exceptions;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.app.Activity;
-import android.content.Intent;
 
-public class Portfolio extends Activity {
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.content.Context;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_portfolio);
-        
+//usually, subclasses of AsyncTask are declared inside the activity class.
+//that way, you can easily modify the UI thread from here
+public class Download_Thread extends AsyncTask<String, Integer, String> {
 
-        
-        //execute this when the download must be fired
- 		Download_Thread downloadFile = new Download_Thread(this);
- 		downloadFile.execute("http://finance.yahoo.com/d/quotes.csv?s=BP.L+EXPN.L+HSBA.L+MKS.L+SN.L&f=l9");
-        
+	String success;
+	public Context ourContext;
+	
+	public Portfolio activty;
+	
+	
+public Download_Thread(Context c) {
+		// TODO Auto-generated constructor stub
+	 
+	ourContext = c;
+	 
+	}
+
+
+@Override
+protected void onPreExecute( ) {
+
+
+	
+
+}
+
+
+@Override
+protected String doInBackground(String... sUrl) {
+	   
+	   
+	   
+   try {
+       URL url = new URL(sUrl[0]);
+       URLConnection connection = url.openConnection();
+       connection.connect();
+
+       //String FILENAME = "Quotes.txt";
+       String path = Environment.getExternalStorageDirectory().getPath();
+       File file = new File(path,"Quotes.csv");
+
        
-        
-        //BUTTON CONTROL STARTS HERE................
-        
-        Button btnLeft = (Button) findViewById(R.id.btnLeft);
-        Button btnRight = (Button) findViewById(R.id.btnRight);
-        
-        
-        btnLeft.setOnClickListener(new View.OnClickListener() {
-        	
-        	public void onClick(View v) {
-				// TODO Auto-generated method stub
-				startActivity(new Intent("com.example.unhandled_exceptions_working.WEEKLY_PROFIT_LOSS"));
+       // download the file
+       InputStream input = new BufferedInputStream(url.openStream());
+       //OutputStream output = new FileOutputStream("/sdcard/Quotes.txt");
+       OutputStream output = new FileOutputStream(file);
+       
 
-				
-			}
-        	
-        });
-        
-        
-        
-        btnRight.setOnClickListener(new View.OnClickListener() {
-        	
-        	public void onClick(View v) {
-				// TODO Auto-generated method stub
-				startActivity(new Intent("com.example.unhandled_exceptions_working.TOTAL_WORTH"));
-				
-				
-			}
-        	
-        });
-        
-        
-        //BUTTON CONTROL STOPS HERE.................
-        
-        
-    }
+       byte data[] = new byte[1024];
+       int count;
+       while ((count = input.read(data)) != -1) {
+
+      	 output.write(data, 0, count);
+       }
+
+       output.flush();
+       output.close();
+       input.close();
+       
+       success = "Download Successful !!!";
+
+       
+   } catch (Exception e) {
+	   success = "Download Unsuccessful !!!";
+   }
    
+   return success;
     
-    
+
+}
+
+
+
+
+@Override
+protected void onPostExecute(String success) {
+
+	//((Portfolio) ourContext).updateScreen(success);
+	
+	   
+}
+
+
 }
