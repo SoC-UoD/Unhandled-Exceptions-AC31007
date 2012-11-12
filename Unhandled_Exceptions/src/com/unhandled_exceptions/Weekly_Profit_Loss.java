21fb9654882028773c21de0939742b722070a2dc
Scott Reid <sfreid@dundee.ac.uk>
2012-11-12 17:42:16 +0000

    changes made to display a dialog for download of dat

--- Unhandled_Exceptions/src/com/unhandled_exceptions/Weekly_Profit_Loss.java
+++ Unhandled_Exceptions/src/com/unhandled_exceptions/Weekly_Profit_Loss.java
@@ -7,13 +7,12 @@ import android.app.Activity;
 import android.content.Intent;
 
 
-public class Weekly_Profit_Loss extends Activity {
+public class Total_Worth extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_weekly_profit_loss);
-        
+        setContentView(R.layout.activity_total_worth);
         
         Button btnLeft = (Button) findViewById(R.id.btnLeft);
         Button btnRight = (Button) findViewById(R.id.btnRight);
@@ -23,7 +22,6 @@ public class Weekly_Profit_Loss extends Activity {
         	
         	public void onClick(View v) {
 				// TODO Auto-generated method stub
-				startActivity(new Intent("com.example.unhandled_exceptions_working.TOTAL_WORTH"));
 				finish();
 			}
         	
@@ -35,10 +33,12 @@ public class Weekly_Profit_Loss extends Activity {
         	
         	public void onClick(View v) {
 				// TODO Auto-generated method stub
+				startActivity(new Intent("com.example.unhandled_exceptions_working.WEEKLY_PROFIT_LOSS"));
 				finish();
 			}
         	
         });
+        
         
         
         
